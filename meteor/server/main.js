a6adce49b52666b3498efbb3bc621db4523908f5
Jørgen Vatle <Jorgen@WcAServices.net>
2018-09-26 09:49:23 +0300

    Allow access to Meteor from cross-origin requests from localhost:8080

--- meteor/server/main.js
+++ meteor/server/main.js
@@ -4,6 +4,11 @@ import { onPageLoad } from "meteor/server-render";
 Meteor.startup(() => {
   // Code to run on server startup.
   console.log(`Greetings from ${module.id}!`);
+
+    WebApp.rawConnectHandlers.use(function(req, res, next) {
+        res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
+        return next();
+    });
 });
 
 onPageLoad(sink => {
