4da1031c77b343a6763a53c6e66ceabeac5d9d9c
Jørgen Vatle <Jorgen@WcAServices.net>
2018-09-26 10:37:55 +0300

    Remove origin access control headers from Meteor startup.

--- meteor/server/main.js
+++ meteor/server/main.js
@@ -1,8 +1,5 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
-    WebApp.rawConnectHandlers.use(function(req, res, next) {
-        res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
-        return next();
-    });
+
 });
