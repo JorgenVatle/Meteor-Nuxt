a7fa19288fa075510629a3625323d2a90b0a797b
Jørgen Vatle <Jorgen@WcAServices.net>
2018-09-26 10:35:22 +0300

    Re-install Meteor scaffolding.

--- meteor/server/main.js
+++ meteor/server/main.js
@@ -1,20 +1,8 @@
-import { Meteor } from "meteor/meteor";
-import { onPageLoad } from "meteor/server-render";
+import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
-  // Code to run on server startup.
-  console.log(`Greetings from ${module.id}!`);
-
     WebApp.rawConnectHandlers.use(function(req, res, next) {
         res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
         return next();
     });
-});
-
-onPageLoad(sink => {
-  // Code to run on every request.
-  sink.renderIntoElementById(
-    "server-render-target",
-    `Server time: ${new Date}`
-  );
 });
