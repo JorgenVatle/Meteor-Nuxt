5017830e57f61b209f530f6e5cf5a0610cfcd7a1
Jørgen Vatle <Jorgen@WcAServices.net>
2018-09-29 06:53:13 +0300

    Add Meteor messages publication.

---
+++ meteor/imports/api/messages/server/publications.js
@@ -0,0 +1,15 @@
+import { Meteor } from 'meteor/meteor';
+import Messages from '../messages';
+
+Meteor.startup(() => {
+
+    Meteor.publish('messages', () => {
+        return Messages.find({}, {
+            sort: {
+                createdAt: -1,
+            },
+            limit: 50,
+        })
+    });
+
+});
