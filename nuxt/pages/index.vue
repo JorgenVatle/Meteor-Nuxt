55aeef8eb381645e0200724e2c11e6607b682fde
Jørgen Vatle <Jorgen@WcAServices.net>
2020-09-28 14:18:17 +0300

    Add silent message posting functionality.

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="h-screen w-screen flex justify-center items-center">
-    <h1 class="m-auto text-2xl">Hello, world!</h1>
     <div>
-      <p class="my-4" v-for="message in messages">{{ message }}</p>
+      <h1 class="text-2xl mb-8 text-center">Hello, world!</h1>
+      <div>
+        <p class="my-4" v-for="message in messages">{{ message }}</p>
+      </div>
+      <form class="flex" @submit.prevent="sendMessage()">
+        <input v-model="messageInput" placeholder="Send a message!" class="bg-gray-800 px-4 py-1 flex-1 rounded-l-lg shadow outline-none">
+        <button type="submit" class="bg-indigo-700 text-white font-medium px-6 py-2 rounded-r-lg">
+          Send
+        </button>
+      </form>
     </div>
   </div>
 </template>
@@ -16,7 +24,11 @@ export default defineComponent({
     Meteor.subscribe('messages');
 
     return {
+      messageInput: '',
       messages: Meteor.collection('messages').reactive().data(),
+      sendMessage() {
+        return Meteor.call('message.post', this.messageInput);
+      }
     }
   }
 })
