ca8fb4ceeace878a4bcbbc4b9bf0a5f982d343e9
Jørgen Vatle <Jorgen@WcAServices.net>
2020-09-28 14:20:56 +0300

    Add VueCompositionApi plugin to Nuxt.

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -28,6 +28,7 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
+    '@/plugins/VueCompositionApi'
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
