b7df63b1b8fdbc590082af6c3b0ba9ec1fb34ac7
Jørgen Vatle <Jorgen@WcAServices.net>
2020-09-28 13:44:44 +0300

    Add Meteor module.

---
+++ nuxt/assets/typescript/Meteor.ts
@@ -0,0 +1,15 @@
+import SimpleDDP from "simpleddp";
+import WebSocket from "isomorphic-ws";
+
+const API_URL = <string>process.env.API_URL;
+
+const Meteor = new SimpleDDP({
+  endpoint: API_URL,
+  SocketConstructor: WebSocket,
+});
+
+Meteor.on('connected', () => console.info('Successfully connected to Meteor running on %s !', API_URL));
+Meteor.on('disconnected', () => console.warn('Lost connection to Meteor!'));
+Meteor.on('error', (e) => console.error('Encountered an error with connection to Meteor DDP!', e));
+
+export default Meteor;
