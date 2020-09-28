b4629a61ff6726d35a25b7732691d92d87acbe37
Jørgen Vatle <Jorgen@WcAServices.net>
2020-09-28 13:51:26 +0300

    Add "best guess" implementation of reactive Meteor data to index page.

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="h-screen w-screen flex">
+  <div class="h-screen w-screen flex justify-center items-center">
     <h1 class="m-auto text-2xl">Hello, world!</h1>
+    <div>
+      <p class="my-4" v-for="message in messages">{{ message }}</p>
+    </div>
   </div>
 </template>
+
+<script>
+import { defineComponent } from '@vue/composition-api';
+import Meteor from 'assets/typescript/Meteor';
+
+export default defineComponent({
+  setup() {
+    Meteor.subscribe('messages');
+
+    return {
+      messages: Meteor.collection('messages').reactive().data();
+    }
+  }
+})
+</script>
