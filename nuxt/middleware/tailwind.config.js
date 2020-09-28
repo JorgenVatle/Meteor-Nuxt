93204939b69fc5bde5249fced5eece6acf48ff6b
Jørgen Vatle <Jorgen@WcAServices.net>
2020-09-28 13:00:17 +0300

    Add Tailwind config file.

---
+++ nuxt/middleware/tailwind.config.js
@@ -0,0 +1,21 @@
+/*
+** TailwindCSS Configuration File
+**
+** Docs: https://tailwindcss.com/docs/configuration
+** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+*/
+module.exports = {
+  theme: {
+    extend: {
+      borderRadius: {
+        xl: '1rem',
+      },
+
+      fontFamily: {
+        article: 'Lato, Helvetica Neue, Helvetica, sans-serif'
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
