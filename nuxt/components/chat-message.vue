b3ac17e12c5d4010a569ee30c10483b4a43ea081
Jørgen Vatle <Jorgen@WcAServices.net>
2020-10-19 07:26:14 +0300

    Move message rendering to dedicated Vue component.

---
+++ nuxt/components/chat-message.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex">
+    <img :src="message.user.picture.thumbnail" class="h-8 rounded-full mr-4">
+    <div>
+      <span class="font-medium block text-gray-600 leading-none mb-1">{{ message.user.name }}</span>
+      <p class="text-gray-200">{{ message.content }}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "@vue/composition-api";
+
+export default defineComponent({
+  name: 'chat-message',
+  props: {
+    message: String,
+  },
+
+  setup(props, context) {
+    console.log(context);
+  }
+})
+</script>
--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -2,24 +2,24 @@
   <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
     <div class="max-w-2xl mx-auto mt-32">
       <div class=" bg-gray-900 rounded-lg shadow-2xl p-4">
+
+        <!-- Heading -->
         <h1 class="text-2xl mb-8 text-center">Meteor Chat, but on Nuxt!</h1>
+
+        <!-- Messages -->
         <div style="height: 500px;" class="overflow-auto max-w-2xl" ref="messageOutput">
-          <div class="py-4 flex" v-for="message in messages" :key="message.id">
-            <img :src="message.user.picture.thumbnail" class="h-8 rounded-full mr-4">
-            <div>
-              <span class="font-medium block text-gray-600 leading-none mb-1">{{ message.user.name }}</span>
-              <p class="text-gray-200">{{ message.content }}</p>
-            </div>
-          </div>
+          <chat-message class="py-4" v-for="message in messages" :message="message" :key="message.id" />
         </div>
+
+        <!-- Send Message -->
         <form class="flex pt-2" @submit.prevent="sendMessage()">
           <input v-model="messageInput" placeholder="Send a message!" class="bg-gray-800 px-4 py-1 flex-1 rounded-l-lg shadow outline-none" autofocus="autofocus">
           <button type="submit" class="bg-indigo-800 text-white font-medium px-6 py-2 rounded-r-lg">
             Send
           </button>
         </form>
-      </div>
 
+      </div>
     </div>
   </div>
 </template>
@@ -27,8 +27,10 @@
 <script>
 import { computed, defineComponent, watch } from '@vue/composition-api';
 import Meteor from 'assets/typescript/Meteor';
+import ChatMessage from '~/components/chat-message';
 
 export default defineComponent({
+  components: { ChatMessage },
   setup(props, context) {
     Meteor.subscribe('messages');
     const messages = Meteor.collection('messages').fetch();
