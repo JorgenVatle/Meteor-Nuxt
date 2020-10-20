b288093f719ee5ac22a53f188a1c214fae673fb6
Jørgen Vatle <Jorgen@WcAServices.net>
2020-10-20 15:38:33 +0300

    Add mup deploy config.

---
+++ meteor/.deploy/mup.js
@@ -0,0 +1,40 @@
+module.exports = {
+  servers: {
+    one: {
+      host: '__API_SERVER_HOST__',
+      username: 'root',
+    }
+  },
+
+  app: {
+    name: 'Meteor-Nuxt',
+    path: '../',
+
+    servers: {
+      one: {},
+    },
+
+    buildOptions: {
+      serverOnly: true,
+    },
+
+    env: {
+      ROOT_URL: 'https://meteor-nuxt-api.wcaservices.net',
+      MONGO_URL: 'mongodb://mongodb/meteor',
+      MONGO_OPLOG_URL: 'mongodb://mongodb/local',
+    },
+
+    docker: {
+      image: 'abernix/meteord:node-12-base',
+    },
+
+    enableUploadProgressBar: false
+  },
+
+  mongo: {
+    version: '3.4.1',
+    servers: {
+      one: {}
+    }
+  },
+};
